cb8008f009867f72ff33a650dbe1db327b20fbe7
Frejvdb <Frva2200@student.miun.se>
2023-09-05 12:58:39 +0200

    GameViewModel on mainwindow startup

--- PacMan/ViewModels/GameViewModel.cs
+++ PacMan/ViewModels/GameViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PacMan.Views.Components;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,27 @@ namespace PacMan.ViewModels
 {
     internal class GameViewModel : BaseViewModel
     {
+        public ObservableCollection<GameMapPiece>? GameMap { get; private set; }
 
+        private const int _mapSize = 20;
+
+        public GameViewModel()
+        {
+            CreateGameMap();
+        }
+
+        private void CreateGameMap()
+        {
+            GameMap = new ObservableCollection<GameMapPiece>();
+            for (int x = 0; x < _mapSize; x++)
+            {
+                for (int y = 0; y < _mapSize; y++)
+                {
+                    var piece = new GameMapPiece();
+                    GameMap.Add(piece);
+                }
+            }
+
+        }
     }
 }
--- PacMan/Views/MainWindow.xaml.cs
+++ PacMan/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PacMan.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace PacMan
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
     }
 }
