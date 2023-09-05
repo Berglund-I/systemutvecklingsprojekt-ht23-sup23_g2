b0a404f42fe5e0e6910f0560f770cc7435d88f64
Robin Larsson <rola2200@student.miun.se>
2023-09-05 10:44:07 +0200

    enums movement

---
+++ PacMan/Enums/Movement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacMan.Enums
+{
+    internal enum Movement
+    {
+        Up,
+        Down,
+        Left,
+        Right,
+    }
+}
