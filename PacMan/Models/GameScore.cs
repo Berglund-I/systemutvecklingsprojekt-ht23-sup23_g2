13d95799995846fa84b2bce225d2eadc1903812e
Robin Larsson <rola2200@student.miun.se>
2023-09-06 11:56:48 +0200

    Added coins entities and other classes for future game entities

---
+++ PacMan/Models/GameScore.cs
@@ -0,0 +1,24 @@
+﻿using PacMan.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacMan.Models
+{
+    class GameScore : BaseViewModel
+    {
+        public GameScore(int score) 
+        {
+            Score = score;
+        }
+        public int Score { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        
+    }
+}
---
+++ PacMan/ViewModels/PlayerViewModel.cs
@@ -0,0 +1,19 @@
+﻿using PacMan.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacMan.ViewModels
+{
+    internal abstract class PlayerViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// Count of the total amount of score in the game
+        /// </summary>
+        public ObservableCollection<GameScore> TotalScore { get; private set; } = new ObservableCollection<GameScore>();
+
+    }
+}
