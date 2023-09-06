b059de9613cf79bdf674b62cea0dfaecc0b2a27c
blisan77 <bilnor-0@student.ltu.se>
2023-09-06 14:37:02 +0200

    Minor changes

--- PacMan/ViewModels/Ghosts/GhostViewModel.cs
+++ PacMan/ViewModels/Ghosts/GhostViewModel.cs
@@ -8,6 +8,6 @@ namespace PacMan.ViewModels.Ghosts
 {
     class GhostViewModel : BaseViewModel
     {
-        public int GhostSize { get; set; } = 50;
+        public int GhostSize { get; set; } = 40;
     }
 }
