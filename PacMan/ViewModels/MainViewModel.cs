4478a1b7901439e6325b259e81a79539d05cc2c6
Berglund-I <idbe2204@student.miun.se>
2023-09-06 13:40:13 +0200

    6/9

---
+++ PacMan/ViewModels/MainViewModel.cs
@@ -0,0 +1,61 @@
+﻿using PacMan.Commands;
+using PacMan.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace PacMan.ViewModels
+{
+    class MainViewModel : BaseViewModel
+    {
+        //public BaseViewModel CurrentViewModel { get; set; } = new GameViewModel(); Probably isnt needed
+        private UserControl _currentView;
+
+        public MainViewModel()
+        {
+            StartGameCommand = new RelayCommand(StartGame);
+        }
+
+        public UserControl CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                if (_currentView != value)
+                {
+                    _currentView = value;
+                }
+            }
+        }
+
+
+
+        public RelayCommand StartGameCommand { get; private set; }
+
+
+        private void StartGame(object parameter)
+        {
+            string playerName = parameter as string;
+
+
+            // Creating a new instance of GameView
+            GameView gameView = new GameView(); 
+                                             
+
+            // Changing the whole conntent in MainWindow to GameView
+            MainWindow mainWindow = Application.Current.MainWindow as MainWindow;
+            if (mainWindow != null)
+            {
+                mainWindow.Content = gameView; 
+                                              
+                                              
+            }
+
+            MessageBox.Show($"Nu börjar spelet {playerName}");
+        }
+    }
+}
