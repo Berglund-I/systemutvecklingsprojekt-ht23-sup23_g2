5f23a2e8e39d26877c0e85e894f0554ea94cf1de
Robin Larsson <rola2200@student.miun.se>
2023-09-14 12:55:14 +0200

    Size and data of coin

---
+++ PacMan/ViewModels/GoldCoinViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace PacMan.ViewModels
+{
+    class GoldCoinViewModel : BaseViewModel
+    {
+
+        public int CoinSize { get; set; } = 20;
+        public GoldCoinViewModel() 
+        {
+            
+        }
+    }
+}
--- PacMan/ViewModels/PlayerViewModel.cs
+++ PacMan/ViewModels/PlayerViewModel.cs
@@ -1,5 +1,4 @@
-﻿using PacMan.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -13,7 +12,7 @@ namespace PacMan.ViewModels
         /// <summary>
         /// Count of the total amount of score in the game
         /// </summary>
-        public ObservableCollection<GameScore> TotalScore { get; private set; } = new ObservableCollection<GameScore>();
+        public ObservableCollection<GoldCoinViewModel> TotalScore { get; private set; } = new ObservableCollection<GoldCoinViewModel>();
 
         public int PlayerLives { get; set; } = 4;
          
