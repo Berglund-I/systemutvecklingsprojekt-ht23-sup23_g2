22a059f6c5ddc6c3fa511e75122d83a7f87bf133
Frejvdb <Frva2200@student.miun.se>
2023-09-19 14:10:22 +0200

    Flyttat movecontrol för MainCharacter till GameVIewModel. Tagit bort kod som ej längre används.

--- PacMan/ViewModels/GameViewModel.cs
+++ PacMan/ViewModels/GameViewModel.cs
@@ -13,47 +13,88 @@ using PacMan.ViewModels.Ghosts;
 using PacMan.ViewModels;
 using PacMan.Enums;
 using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace PacMan.ViewModels
 {
     internal class GameViewModel : BaseViewModel
     {
-        public MainCharacterViewModel MainCharacterViewModel { get; set; } = new MainCharacterViewModel();
         public MainCharacter MainCharacter { get; set; } = new MainCharacter();
         public GhostViewModel Ghosts { get; set; } = new GhostViewModel();
         public GhostBlue GhostBlue { get; set; } = new GhostBlue();
         public GhostBlue GhostBlue2 { get; set; } = new GhostBlue();
         public int GhostSize { get; set; }
         public int McSize { get; set; }
+        public static Movement MovementDirection { get; set; }
+        public ICommand LeftPressedCommand { get; set; }
+        public ICommand RightPressedCommand { get; set; }
+        public ICommand UpPressedCommand { get; set; }
+        public ICommand DownPressedCommand { get; set; }
+        int timerSpeed = 100;
+        private int movementSpeed = 10;
         //public ObservableCollection<GameMapPiece>? GameMap { get; private set; }
-        
-        
+        private readonly DispatcherTimer timer = new();
+
 
         //private const int _mapSize = 20;
 
         public GameViewModel()
         {
-            McSize = MainCharacterViewModel.McSize;
             GhostSize = Ghosts.GhostSize;
-           
+            McSize = MainCharacter.Size;
+            LeftPressedCommand = new RelayCommand(x => LeftPressed());
+            RightPressedCommand = new RelayCommand(x => RightPressed());
+            UpPressedCommand = new RelayCommand(x => UpPressed());
+            DownPressedCommand = new RelayCommand(x => DownPressed());
+            MovementDirection = Movement.Down;
+
+            timer.Interval = TimeSpan.FromMilliseconds(timerSpeed);
+            
+            timer.Tick += MainCharacterMovementTimer;
+            timer.Start();
+
         }
-        
 
-        /// <summary>
-        /// Generates a grid in GameView of _mapSize size
-        /// </summary>
-        //private void CreateGameMap()
-        //{
-        //    GameMap = new ObservableCollection<GameMapPiece>();
-        //    for (int x = 0; x < _mapSize; x++)
-        //    {
-        //        for (int y = 0; y < _mapSize; y++)
-        //        {
-        //            var piece = new GameMapPiece();
-        //            GameMap.Add(piece);
-        //        }
-        //    }
+        private void MainCharacterMovementTimer(object? sender, EventArgs e)
+        {
+            switch (MovementDirection)
+            {
+                case Movement.Up:
+                    MainCharacter.YCoordinate -= movementSpeed;
+                    break;
+                case Movement.Down:
+                    MainCharacter.YCoordinate += movementSpeed;
+                    break;
+                case Movement.Left:
+                    MainCharacter.XCoordinate -= movementSpeed;
+                    break;
+                case Movement.Right:
+                    MainCharacter.XCoordinate += movementSpeed;
+                    break;
+                default:
+                    break;
+            }
+        }
 
-        //}
+        private void DownPressed()
+        {
+            MovementDirection = Movement.Down;
+        }
+
+        private void UpPressed()
+        {
+            MovementDirection = Movement.Up;
+        }
+
+        private void RightPressed()
+        {
+            MovementDirection = Movement.Right;
+        }
+
+        private void LeftPressed()
+        {
+            MovementDirection = Movement.Left;
+
+        }
     }
 }
