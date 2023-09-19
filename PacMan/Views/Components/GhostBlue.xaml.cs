cfbc4ac4ef55e3ecb08bef8ab2d9155bfc101790
blisan77 <bilnor-0@student.ltu.se>
2023-09-19 10:07:50 +0200

    .

--- PacMan/Views/Components/GhostBlue.xaml.cs
+++ PacMan/Views/Components/GhostBlue.xaml.cs
@@ -24,7 +24,7 @@ namespace PacMan.Views.Components
         public GhostBlue()
         {
             InitializeComponent();
-            //DataContext = new GhostViewModel();
+            
         }
     }
 }
